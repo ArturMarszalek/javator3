b6856abbc6669fd68f477e4e1d96f9b33e0b49b0
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-06-30 09:55:58 +0200

    computer choice done

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Computer {
 
-    
+    private static Elements computerChoise (){
+
+        Random random = new Random();
+
+        int computerChoice = random.nextInt(3) +1;
+
+        switch  (computerChoice){
+            case 1:
+                return Elements.PAPIER;
+            case 2:
+                return Elements.NOZYCE;
+        }
+
+        return Elements.KAMIEN;
+    }
 
 }
